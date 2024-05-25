29eeb6ebe0e4f205669d494026642d2bfe8f1105
kusuyun <116808443+Kusu-Nose@users.noreply.github.com>
2024-05-25 14:45:57 +0900

    たくあん追加

--- EL4S_0525_team5/Assets/GimmickBase.cs
+++ EL4S_0525_team5/Assets/GimmickBase.cs
@@ -2,9 +2,18 @@ using UnityEngine;
 
 public class GimmickBase : MonoBehaviour
 {
+    protected enum CuttingState
+    {
+        NotCut,
+        Cutting,
+        CutComplete,
+    }
+
     protected static readonly Vector2 CUT_POSITION = new Vector2(-10, 0);
     [SerializeField] protected float _duration = 2.0f;
     [SerializeField] protected GameObject _objectFragment;
+
+    protected CuttingState cuttingState = CuttingState.NotCut;
 
     private void Start()
     {
@@ -30,8 +39,19 @@ public class GimmickBase : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Knife")) OnHitKnifeEnter(collision);
     }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Knife")) OnHitKnifeStay(collision);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Knife")) OnHitKnifeExit(collision);
+    }
 
     protected virtual void OnHitKnifeEnter(Collider2D collision) { }
+    protected virtual void OnHitKnifeStay(Collider2D collision) { }
+    protected virtual void OnHitKnifeExit(Collider2D collision) { }
 
     protected virtual void OnStart() { }
     protected virtual void OnUpdate() { }
