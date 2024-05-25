3af114a81533a157c3bfbe87a326e864df674ebd
DESKTOP-1PSHM9A\Administrator <atroriass@gmail.com>
2024-05-25 12:25:26 +0800

    UI Staff

---
+++ EL4S_0525_team5/Assets/Scripts/ScoreTable.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class ScoreTable : MonoBehaviour
+{
+    public static ScoreTable Instance;
+
+    TextMeshProUGUI scoreText;
+    static int score=0;
+
+    void Awake()
+    {
+        ScoreTable.Instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        scoreText = GetComponent<TextMeshProUGUI>();
+        if(scoreText != null )
+        scoreText.text = string.Format("{0:d5}",score);
+    }
+
+
+    public void ScoreIncrease(int i)
+    {
+        score += i;
+        scoreText.text = string.Format("{0:d5}", score);
+    }
+    public void Reset()
+    {
+        score = 0;
+    }
+
+    public int GetSocre()
+    {
+        return score;
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+       
+    }
+}
