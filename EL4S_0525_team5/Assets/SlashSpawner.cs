1ec46ba8612c311ab30651b5088b8699cb7f1222
AkrMrkm <kagaroudaze1251@gmail.com>
2024-05-25 15:05:32 +0900

    エフェクトアップデート

--- EL4S_0525_team5/Assets/SlashSpawner.cs
+++ EL4S_0525_team5/Assets/SlashSpawner.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,10 +14,18 @@ public class SlashSpawner : MonoBehaviour
     [SerializeField]
     private float _randomRange;
 
+    private CinemachineImpulseSource _impulseSource;
+
+    private Vector3 _originalPos;
+    private Quaternion _originalRot;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _impulseSource = GetComponent<CinemachineImpulseSource>();
+
+        _originalPos = new Vector3(0f, 0f, -1f);
+        _originalRot = Quaternion.identity;
     }
 
     // Update is called once per frame
@@ -24,14 +33,41 @@ public class SlashSpawner : MonoBehaviour
     {
         if(Input.GetKeyUp(KeyCode.Space)) 
         {
-            // 斬撃エフェクト
-            Instantiate(_effect);
+            GenerateSlashEffect();
 
-            // ザク文字
-            Vector3 randPos = new Vector3(Random.Range(-_randomRange, _randomRange), Random.Range(0, _randomRange), 0f);
-            randPos += transform.position;
-            Quaternion randRot = new Quaternion(0f, 0f, Random.Range(-0.8f, 0.8f), 1f);
-            Instantiate(_zaku, randPos, randRot);
+            GenerateZaku();
         }
+    }
+
+    public void GenerateSlashEffect()
+    {
+        // 斬撃エフェクト
+        Instantiate(_effect);
+    }
+
+    public void GenerateZaku()
+    {
+        // ザク文字
+        Vector3 randPos = new Vector3(Random.Range(-_randomRange, _randomRange), Random.Range(0, _randomRange), 0f);
+        randPos += transform.position;
+        Quaternion randRot = new Quaternion(0f, 0f, Random.Range(-0.8f, 0.8f), 1f);
+        Instantiate(_zaku, randPos, randRot);
+
+        CameraShake();
+    }
+
+    public void CameraShake()
+    {
+        StartCoroutine(_CameraShake());
+    }
+
+    IEnumerator _CameraShake()
+    {
+        _impulseSource.GenerateImpulse();
+
+        yield return new WaitForSeconds(1f);
+
+        Camera.main.transform.position = _originalPos;
+        Camera.main.transform.localRotation = _originalRot;
     }
 }
