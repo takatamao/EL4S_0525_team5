8506fb3b4a6f654100246f6f55c1706bb330e67f
takatamao <yuzu03133@gmail.com>
2024-05-25 11:05:52 +0900

    item

---
+++ EL4S_0525_team5/Assets/Carrot.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Carrot : GimmickBase
+{
+    protected override void OnHitKnifeEnter(Collision collision)
+    {
+        //“–‚½‚Á‚½‚Ì‰‰o
+    }
+}
